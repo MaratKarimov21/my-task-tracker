27aa0350bca95c47cdac26d905b971d9e7beb003
Maratkarimov21 <marat.karimov@flatstack.com>
2021-12-22 18:09:57 +0300

    fix

--- app/models/tasks_relation.rb
+++
@@ -1,6 +0,0 @@
-class TasksRelation < ApplicationRecord
-  belongs_to :blocked, class_name: "Task"
-  belongs_to :blocker, class_name: "Task"
-
-  validates :blocker_id, uniqueness: { scope: :blocked_id }
-end
